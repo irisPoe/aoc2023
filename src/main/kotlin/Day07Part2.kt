25b306660acf0df68a5f90943bfcd3a9ee95d34c
Iris Pöchtrager <iris.poechtrager@yahoo.com>
2023-12-08 14:54:38 +0100

    Add solution for Day07

---
+++ src/main/kotlin/Day07Part2.kt
@@ -0,0 +1,156 @@
+import structure.LinesPuzzle
+import java.util.*
+
+class Day07Part2 : LinesPuzzle() {
+    enum class HandType {
+        FIVE_OF_A_KIND,
+        FOUR_OF_A_KIND,
+        FULL_HOUSE,
+        THREE_OF_A_KIND,
+        TWO_PAIR,
+        ONE_PAIR,
+        HIGH_CARD
+    }
+
+    enum class CardType {
+        CardJ,
+        Card2,
+        Card3,
+        Card4,
+        Card5,
+        Card6,
+        Card7,
+        Card8,
+        Card9,
+        CardT,
+        CardQ,
+        CardK,
+        CardA
+    }
+
+    override fun solve(lines: List<String>): String {
+        val handsWithBids = mutableMapOf<Hand, Long>()
+
+        for (line in lines) {
+            val splitLine = line.split(" ")
+            val hand = Hand(splitLine[0].map { CardType.valueOf("Card$it") })
+            handsWithBids[hand] = splitLine[1].toLong()
+        }
+
+        val sortedHandsWithBids = handsWithBids.toList().sortedBy { it.first }
+        var result = 0L
+        sortedHandsWithBids.forEachIndexed { index, handWithBid ->
+            result += handWithBid.second * (index+1)
+        }
+
+        return result.toString()
+    }
+
+    private class Hand(val hand: List<CardType>): Comparable<Hand> {
+        val handType: HandType
+        val cardOccurrences: List<CardOccurrence>
+
+        init {
+            this.cardOccurrences = getCardCountList()
+            this.handType = getHandType(cardOccurrences)
+        }
+
+        fun winsAgainst(otherHand: Hand): Boolean {
+            return if (otherHand.handType == this.handType) {
+                var indexToCompare = 0
+
+                while (this.hand[indexToCompare] == otherHand.hand[indexToCompare]) {
+                    indexToCompare++
+                }
+
+                if (indexToCompare >= this.hand.size) {
+                    throw Error("two equal hands given")
+                }
+
+                this.hand[indexToCompare] > otherHand.hand[indexToCompare]
+            } else {
+                this.handType < otherHand.handType
+            }
+        }
+
+        private fun getCardCountList(): List<CardOccurrence> {
+            val cardCountMap = mutableMapOf<CardType, Int>()
+
+            for (cardType in hand) {
+                if (cardCountMap.containsKey(cardType)) {
+                    cardCountMap[cardType] = cardCountMap[cardType]!! + 1
+                } else {
+                    cardCountMap[cardType] = 1
+                }
+            }
+
+            return cardCountMap.toList().map { CardOccurrence(it.first, it.second) }
+        }
+
+        private fun getHandType(cardOccurrences: List<CardOccurrence>): HandType {
+            val highestAmountsOfCards = getHighestAmountsOfCards(cardOccurrences)
+            return when (highestAmountsOfCards.highestAmount) {
+                5 -> HandType.FIVE_OF_A_KIND
+                4 -> HandType.FOUR_OF_A_KIND
+                3 -> {
+                    if (highestAmountsOfCards.secondHighestAmount == 2) {
+                        HandType.FULL_HOUSE
+                    } else {
+                        HandType.THREE_OF_A_KIND
+                    }
+                }
+                2 -> {
+                    if (highestAmountsOfCards.secondHighestAmount == 2) {
+                        HandType.TWO_PAIR
+                    } else {
+                        HandType.ONE_PAIR
+                    }
+                }
+                else -> HandType.HIGH_CARD
+            }
+        }
+
+        private fun getHighestAmountsOfCards(cardOccurrences: List<CardOccurrence>): HighestAmoundOfCardsResult {
+            val sortedCardOccurrences = cardOccurrences.sortedByDescending { it.amount }
+            val numberOfJokers = sortedCardOccurrences.firstOrNull { it.card == CardType.CardJ }?.amount ?: 0
+
+            if (sortedCardOccurrences[0].amount == 5) {
+                return HighestAmoundOfCardsResult(5, 0)
+            }
+
+            val highestAmount = if (sortedCardOccurrences[0].card == CardType.CardJ) {
+                sortedCardOccurrences[1].amount + sortedCardOccurrences[0].amount
+            } else {
+                sortedCardOccurrences[0].amount + numberOfJokers
+            }
+
+            if (highestAmount == 5) {
+                return HighestAmoundOfCardsResult(5, 0)
+            }
+
+            val secondHighestAmount =
+                if (sortedCardOccurrences[0].card == CardType.CardJ || sortedCardOccurrences[1].card == CardType.CardJ) {
+                    sortedCardOccurrences[2].amount
+                } else {
+                    sortedCardOccurrences[1].amount
+                }
+
+            return HighestAmoundOfCardsResult(highestAmount, secondHighestAmount)
+        }
+
+        override fun compareTo(other: Hand): Int =
+            if (this.winsAgainst(other)) {
+                1
+            } else {
+                -1
+            }
+
+        private class HighestAmoundOfCardsResult(val highestAmount: Int, val secondHighestAmount: Int)
+    }
+
+    private class CardOccurrence(val card: CardType, val amount: Int)
+
+    companion object {
+
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 val AOC_YEAR = "2023"
-val PUZZLE = Day06Part2()
+val PUZZLE = Day07Part2()
 
 fun main(args: Array<String>) {
     println("Solution for example: ${PUZZLE.solveExample()}")
